14d372475bb35a1aa98817d8234db68fcfce26bd
DuyDo <duydo.dev@gmail.com>
2017-11-20 15:43:05 -0600

    create login sigup modal

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -8,7 +8,7 @@ const Header = (props) => (
         <Login />
         <Signup />
         <h1>SwapCard</h1>
-        <h2>A place to trade your unused and unwanted give cards for retailer you prefer</h2>
+        <h2>A place to trade your unused and unwanted gift cards for retailer you prefer</h2>
         <button className="btn btn-success">Log in</button>
         <button className="btn btn-danger">Sign up</button>
     </div>

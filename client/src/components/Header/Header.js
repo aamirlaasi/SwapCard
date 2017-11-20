7badeacd6526bca43b1b3923bb98af5048aa9ffd
DuyDo <duydo.dev@gmail.com>
2017-11-20 02:11:47 -0600

    finish card modal

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -3,6 +3,7 @@ import React from "react";
 const Header = () => (
     <div className="jumbotron text-center">
         <h1>SwapCard</h1>
+        <h2>A place to trade your unused and unwanted give cards for retailer you prefer</h2>
     </div>
 )
 

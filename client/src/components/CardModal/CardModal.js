856e4a02d72b8a4ca7e29e27f66095488279af38
DuyDo <duydo.dev@gmail.com>
2017-11-22 00:02:43 -0600

    create scroll bar for CardModal

--- client/src/components/CardModal/CardModal.js
+++ client/src/components/CardModal/CardModal.js
@@ -1,13 +1,14 @@
 import React from "react";
 import Modal from "react-modal";
+import Carousel from "../Carousel";
 import "./CardModal.css";
 const style = {
     content : {
         position                   : 'absolute',
-        top                        : '40px',
+        top                        : '20px',
         left                       : '100px',
         right                      : '100px',
-        bottom                     : '40px',
+        bottom                     : '20px',
         border                     : '2px solid #ccc',
         background                 : '#fff',
         overflow                   : 'auto',
@@ -24,30 +25,40 @@ const CardModal = (props) => (
             onRequestClose={props.closeModal}
             contentLabel="Card details"
         >
-            <div  className="cardModal text-center" onClick={props.loadModal}>
-                <h3>Gift Card you wish to trade</h3>
-                {/* <div className="img-container"> */}
-                <img alt={props.store} src={props.fimage} />
-                </div>
-                <div className="content">
-                    <ul>
-                        <li>
-                        <strong>Owner:</strong>
-                        </li>
-                        <li>
-                        <strong>Store:</strong> {props.store}
-                        </li>
-                        <li>
-                        <strong>Price:</strong> {props.price}
-                        </li>
-                        <li>
-                        <strong>ExpDate:</strong> {props.exp}
-                        </li>
-                    </ul>
-                    <button style={{"marginRight": "20px"}} className="btn btn-success" onClick={props.handleTrade}>CLICK TO TRADE</button>
+            <div>
+                <div className="row content">
+                    <h3>Gift Card you wish to trade</h3>
+                    <hr />
+                    <div className="img-container text-center col-lg-8" onClick={props.loadModal}>
+                        <img alt={props.store} src={props.fimage} />
+                    </div>
 
-                    <button className="btn btn-danger" onClick={props.closeModal}>CLOSE</button>
-                {/* </div> */}
+                    <div className="cardDetail col-lg-4">
+                        <ul>
+                            <li>
+                            <strong>Owner:</strong>
+                            </li>
+                            <li>
+                            <strong>Store:</strong> {props.store}
+                            </li>
+                            <li>
+                            <strong>Price:</strong> {props.price}
+                            </li>
+                            <li>
+                            <strong>ExpDate:</strong> {props.exp}
+                            </li>
+                        </ul>
+                        <button style={{"marginRight": "20px"}} className="btn btn-success" onClick={props.handleTrade}>CLICK TO TRADE</button>
+                        <button className="btn btn-danger" onClick={props.closeModal}>CLOSE</button>
+                    </div>
+                    <hr /> 
+                </div>
+                <hr />
+                {/* Card that user have */}
+                <div className="row">
+                    <Carousel />
+                </div>                
+
             </div>
         </Modal>
     );
---
+++ client/src/components/Carousel/Carousel.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./Carousel.css";
+
+const Carousel = () => (
+    <div id="carousel">
+        <div class="slide">
+            <img src="http://placehold.it/300x150"/>
+        </div>
+        <div class="slide">
+            <img src="http://placehold.it/300x150"/>
+        </div>
+        <div class="slide">
+            <img src="http://placehold.it/300x150"/>
+        </div>
+        <div class="slide">
+            <img src="http://placehold.it/300x150"/>
+        </div>
+        <div class="slide">
+            <img src="http://placehold.it/300x150"/>
+        </div>
+        <div class="slide">
+            <img src="http://placehold.it/300x150"/>
+        </div>
+    </div>
+)
+
+export default Carousel;

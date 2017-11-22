49738fa8e1ee34a71222cbdac2c7e853724f7d9e
DuyDo <duydo.dev@gmail.com>
2017-11-22 11:17:04 -0600

    update card carousel

--- client/src/components/CardModal/CardModal.js
+++ client/src/components/CardModal/CardModal.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {Component} from "react";
 import Modal from "react-modal";
 import Carousel from "../Carousel";
 import "./CardModal.css";
@@ -19,51 +19,55 @@ const style = {
     }
   };
 
-const CardModal = (props) => (
-        <Modal style={style}
-            isOpen={!!props.selectedCard}
-            onRequestClose={props.closeModal}
-            contentLabel="Card details"
-        >
-            <div>
-                <div className="row content">
-                    <h3>Gift Card you wish to trade</h3>
-                    <hr />
-                    <div className="img-container text-center col-lg-7" onClick={props.loadModal}>
-                        <img alt={props.store} src={props.fimage} />
-                    </div>
+class CardModal extends Component {
 
-                    <div className="cardDetail col-lg-5">
-                        <ul>
-                            <li>
-                            <strong>Owner:</strong>
-                            </li>
-                            <li>
-                            <strong>User rating: 5</strong>
-                            </li>
-                            <li>
-                            <strong>Store:</strong> {props.store}
-                            </li>
-                            <li>
-                            <strong>Price:</strong> {props.price}
-                            </li>
-                            <li>
-                            <strong>ExpDate:</strong> {props.exp}
-                            </li>
-                        </ul>
-                        <button style={{"marginRight": "20px"}} className="btn btn-success" onClick={props.handleTrade}>CLICK TO TRADE</button>
-                        <button className="btn btn-danger" onClick={props.closeModal}>CLOSE</button>
-                    </div>
-                    <hr /> 
-                </div>
-                <hr />
-                {/* Card that user have */}
-                <div className="row">
-                    {true ? <Carousel /> : "Please login or create accout to trade card"}
-                </div>                
-
-            </div>
-        </Modal>
-    );
+    render() {
+        return(
+            <Modal style={style}
+                        isOpen={!!this.props.selectedCard}
+                        onRequestClose={this.props.closeModal}
+                        contentLabel="Card details"
+                    >
+                        <div>
+                            <div className="row content">
+                                <h3>Gift Card you wish to trade</h3>
+                                <hr />
+                                <div className="img-container text-center col-lg-7" onClick={this.props.loadModal}>
+                                    <img alt={this.props.store} src={this.props.fimage} />
+                                </div>
+            
+                                <div className="cardDetail col-lg-5">
+                                    <ul>
+                                        <li>
+                                        <strong>Owner:</strong>
+                                        </li>
+                                        <li>
+                                        <strong>User rating: 5</strong>
+                                        </li>
+                                        <li>
+                                        <strong>Store:</strong> {this.props.store}
+                                        </li>
+                                        <li>
+                                        <strong>Price:</strong> {this.props.price}
+                                        </li>
+                                        <li>
+                                        <strong>ExpDate:</strong> {this.props.exp}
+                                        </li>
+                                    </ul>
+                                    <button style={{"marginRight": "20px"}} className="btn btn-success" onClick={this.props.handleTrade}>CLICK TO TRADE</button>
+                                    <button className="btn btn-danger" onClick={this.props.closeModal}>CLOSE</button>
+                                </div>
+                                <hr /> 
+                            </div>
+                            <hr />
+                            {/* Card that user have */}
+                            <div className="row">
+                                {true ? <Carousel /> : "Please login or create accout to trade card"}
+                            </div>                
+                        </div>
+                    </Modal>
+        )
+    }
+}
 
 export default CardModal;

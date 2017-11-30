34e37b67f4934e7946b76abe0f719b92fd1feed1
DuyDo <duydo.dev@gmail.com>
2017-11-29 23:36:47 -0600

    finish add new card modal

--- client/src/components/AddNewCard/AddNewCard.js
+++ client/src/components/AddNewCard/AddNewCard.js
@@ -1,20 +1,42 @@
 import React, {Component} from "react";
+import Webcam from 'react-webcam';
+
 import Modal from "react-modal";
-import Capture from "../Webcam/Capture";
+// import Capture from "../Webcam/Capture";
+import WebcamCapture from "../WebcamCapture"
+import "./AddNewCard.css"
 
 class AddNewCard extends Component {
     state = {
-        selectWebcam: undefined,
-        front: ""
+        // selectWebcam: undefined,
+        front:"",
+        back: ""
     }
+    setRef = (webcam) => {
+        this.webcam = webcam;
+      }
+    // selectWebcam = (e) => {
+    //     e.preventDefault();
+    //     this.setState({
+    //         selectWebcam: true
+    //     })
+    // }
 
-    selectWebcam = (e) => {
+    captureFront = (e) => {
         e.preventDefault();
+        const imageFront = this.webcam.getScreenshot();
         this.setState({
-            selectWebcam: true
+          front: imageFront,
         })
-    }
+      };
 
+    captureBack = (e) => {
+        e.preventDefault();        
+        const imageBack = this.webcam.getScreenshot();
+        this.setState({
+            back: imageBack,
+        })
+    }; 
     unselectWebcam = (e) => {
         e.preventDefault();
         this.setState({
@@ -32,16 +54,48 @@ class AddNewCard extends Component {
                     <h2>Take picture of front and back, input card details</h2>
                     <hr />
                     <form>
-                        <input type="text" /> Card Name
-                        <input type="text" /> Card Price
-                        <input type="text" /> Exp date
-                        <button onClick={this.selectWebcam}>Capture front of Card</button>
-                        {/* <img src={this.state.front}/> */}
-                        <button>Capture back of Card</button>
-                        <button type="submit">Submit</button>
+                        <div className="row">
+                            <div className="col-lg-2">
+                                <input type="text" className="form-group"  placeholder="Store name" required/>
+                            </div>
+                            <div className="col-lg-2">
+                                <input type="text" className="form-group"  placeholder="Price" required/>
+                            </div>
+                            <div className="col-lg-2">
+                                <input type="text" className="form-group"  placeholder="Exp date" required/>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col-lg-3">
+                                <Webcam
+                                    audio={false}
+                                    height={350}
+                                    ref={this.setRef}
+                                    screenshotFormat="image/jpeg"
+                                    width={350}
+                                />
+                            </div>
+
+                            <div className="cardImage col-lg-3">
+                                <button onClick={this.captureFront}>Capture front of Card</button>
+                                <br />
+                                <img src={this.state.front} alt=""/>
+                            </div>
+
+                            <div className="cardImage col-lg-3">
+                                <button onClick={this.captureBack}>Capture back of Card</button>
+                                <br />
+                                <img src={this.state.back} alt=""/>
+                            </div>
+                        </div>
+                
+                        {this.state.front && this.state.back ? (
+                            <button type="submit">Submit</button>
+                        ): <p>Please capture both front and back of card</p>}
+                        <button onClick={this.props.unselectAddNewCard}>Cancel</button>
                     </form>
                 </div>
-                <Capture unselectWebcam={this.unselectWebcam} selectWebcam={this.state.selectWebcam} front={this.state.front} />
+                <WebcamCapture unselectWebcam={this.unselectWebcam} selectWebcam={this.state.selectWebcam} front={this.state.front} />
             </Modal>
         )
     }
---
+++ client/src/components/WebcamCapture/WebcamCapture.js
@@ -0,0 +1,60 @@
+import Webcam from 'react-webcam';
+import React, {Component} from "react";
+import Modal from "react-modal";
+
+class WebcamCapture extends Component {
+    state = {
+      front: "",
+      selectWebcam: true
+    }
+    setRef = (webcam) => {
+      this.webcam = webcam;
+    }
+
+   
+    capture = (e) => {
+      // e.preventDefault();
+      const imageSrc = this.webcam.getScreenshot();
+      this.setState({
+        front: imageSrc,
+        selectWebcam: false
+      })
+    };
+   
+    render() {
+      return (
+        <Modal
+          front = {this.state.front}
+          isOpen={!!this.props.selectWebcam}
+          onRequestClose={this.props.unselectWebcam}
+        >
+          <div className="row">
+            <div className="col-lg-4">
+              <Webcam
+                audio={false}
+                height={350}
+                ref={this.setRef}
+                screenshotFormat="image/jpeg"
+                width={350}
+              />
+            </div>
+            <div className="col-lg-4">
+              <button onClick={this.capture} selectWebcam={this.state.selectWebcam}>Click here to take picture</button>
+              {this.state.img ? (
+                <div>
+                <img src={this.state.img}/>
+                <br />
+                <button onClick={this.closeWebcam}>Finish</button>
+                  
+                </div>
+              ) : <p>Please take picture</p> }
+            </div>
+            
+          </div>
+      
+      </Modal>
+      );
+    }
+  }
+
+  export default WebcamCapture;

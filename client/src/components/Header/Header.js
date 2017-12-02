d4c69735bdabea19d00d5e3dc29234bd5d8bccae
Aamir Laasi <aamirlaasi@Aamirs-MacBook-Air.local>
2017-12-02 03:14:13 -0600

    Updated Header.js component to work with authorization

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,40 +1,90 @@
 import React, {Component} from "react";
 import "./Header.css";
-import Login from "../Login";
-import Signup from "../Signup";
-import { Link } from "react-router-dom";
+import { Button } from 'react-bootstrap';
+import Auth from "../../Auth/Auth.js";
 
+// import Login from "../Login";
+// import Signup from "../Signup";
+import {Link} from "react-router-dom";
 
 class Header extends Component {
-    state = {
-        selectLogin: undefined
+  // state = {
+  //   selectedLogin: undefined
+  // }
+  // handleLogin = ()=> {
+  //   this.setState({
+  //     selectedLogin:true
+  //   })
+  // }
+  // closeLogin = ()=> {
+  //   this.setState({
+  //     selectedLogin:false
+  //   })
+  // }
+  // handleSignup = ()=> {
+  //   this.setState({
+  //     selectedSignup:true
+  //   })
+  // }
+
+  // closeSignup = ()=> {
+  //   this.setState({
+  //     selectedSignup:false
+  //   })
+  // }
+
+  constructor(props) {
+        super(props)
+        this.auth = new Auth();
     }
-    handleLogin = () => {
-        this.setState({
-            selectLogin: true
-        })
-    }
-    closeLogin = () => {
-        this.setState({
-            selectLogin: false
-        })
-    }
-    render() {
-        return(
-            <div className="jumbotron text-center">
-                <Login closeLogin={this.closeLogin} handleLogin={this.state.selectLogin}/>
-                <Signup />
-                <h1>SwapCard</h1>
-                <h2>A place to trade your unused and unwanted gift cards for retailer you prefer</h2>
-                <button className="btn btn-success" onClick={this.handleLogin} >Log in</button>
-                <button className="btn btn-danger">Sign up</button>
-                {/* creat button to test userprofile page route */}
-                <button className={window.location.pathname === "/" ? "active btn" : ""}>
-                    <Link to="/UserProfile">User Profile - test only</Link>
-                </button>
-            </div>
-        )
-    }
+
+  login() {
+    this.auth.login();
+  }
+
+  logout() {
+    this.auth.logout();
+  }
+
+  render() {
+    // const { isAuthenticated } = this.props.auth;
+
+    return (
+        <div className="jumbotron text-center">
+            {/* <Login closeLogin={this.closeLogin} handleLogin={this.state.selectedLogin} /> */}
+            {/* <Signup closeSignup={this.closeSignup} handleSignup={this.state.selectedSignup} handleSignupSubmit={this.handleSignupSubmit} /> */}
+            <h1>SwapCard</h1>
+            <h2>A place to trade your unused and unwanted gift cards for retailer you prefer</h2>
+            {
+              !this.auth.isAuthenticated() && (
+                  <Button
+                    bsStyle="primary"
+                    className="btn-margin"
+                    onClick={this.login.bind(this)}
+                  >
+                    Log In / Sign-up
+                  </Button>
+                )
+            }
+            {console.log(this)}
+            {
+              this.auth.isAuthenticated() && (
+                  <Button
+                    bsStyle="primary"
+                    className="btn-margin"
+                    onClick={this.logout.bind(this)}
+                  >
+                    Log Out
+                  </Button>
+                )
+            }
+
+            {/* <button className="btn btn-success" onClick={this.handleLogin}>Log in</button> */}
+            {/* <button className="btn btn-danger" onClick={this.handleSignup}>Sign up</button> */}
+        </div>
+    )
+  }
 }
+
 
 export default Header;

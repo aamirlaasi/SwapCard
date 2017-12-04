b9d719c58157e23f16dbd49602939bf65c43d7c4
DuyDo <duydo.dev@gmail.com>
2017-12-03 20:15:32 -0600

    access local storage from User component

--- client/src/pages/User/User.js
+++ client/src/pages/User/User.js
@@ -2,7 +2,10 @@ import React, {Component} from "react";
 import UserProfile from "../../components/UserProfile"
 
 class User extends Component {
-    
+    componentDidMount() {
+        console.log("This is from Pages/User component");
+        console.log(localStorage.getItem("profile"));
+    }
     render() {
         return(
             <UserProfile />

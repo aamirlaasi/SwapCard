1b91aac875262085d07eab39ab1792fd6acc3a3b
Sagabootcamp <sagarika.susarla05@gmail.com>
2017-12-04 10:10:54 -0600

    added route in server.js

--- server.js
+++ server.js
@@ -14,10 +14,14 @@ const PORT = process.env.PORT || 3001;
 // Configure body parser for AJAX requests
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-// Serve up static assets
-app.use(express.static("client/build"));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 // Add routes, both API and view
 app.use(routes);
+// Serve up static assets
+app.use(express.static("client/build"));
 
 
 // Set mongoose to leverage built in JavaScript ES6 Promises

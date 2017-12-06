f62ff40b3c458f68b402be3d561a1979483e8db6
DuyDo <duydo.dev@gmail.com>
2017-12-06 02:13:41 -0600

    add more user to userDB.js

--- scripts/userDB.js
+++ scripts/userDB.js
@@ -37,6 +37,29 @@ const UserSeed = [
   },
   {
     fullname: "Duy Do",
+    email:"duydo@gmail.com",
+    password: "12345",
+    userCreated: new Date(Date.now()),
+    giftcard:[{
+      store: "Walmart",
+      price: 100,
+      fimage: "http://entertainkidsonadime.com/wp-content/uploads/2016/03/IMG_49692-2.jpg",
+      bimage: "https://i.colnect.net/f/2468/534/Freeloader-back.jpg",
+      exp: new Date(Date.now()),
+      stored:false
+    },
+      {
+      store: "Starbucks",
+      price: 50,
+      fimage: "https://www.grovia.com/media/catalog/product/cache/2/image/650x/040ec09b1e35df139433887a97daa66f/s/t/starbucks_giftcard.png",
+      bimage: "https://i.colnect.net/f/1247/826/10-Jahre-Starbucks-Deutschland-6078-back.jpg",
+      exp: new Date(Date.now()),
+      stored:false
+    }
+    ]
+  },
+  {
+    fullname: "Duy Do 2",
     email:"leduypc1988@gmail.com",
     password: "12345",
     userCreated: new Date(Date.now()),

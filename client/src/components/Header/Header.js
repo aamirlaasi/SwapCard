14d967edcfffb607041a5eb9db1e73b215978caf
Dan Byers <dbyers@gmail.com>
2017-12-07 18:45:39 -0600

    CSS Update

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,17 +1,17 @@
 import React, {Component} from "react";
 import "./Header.css";
+import "../Footer/Footer.css";
+import Footer from "../Footer";
 import { Button } from 'react-bootstrap';
 import Auth from "../../Auth/Auth.js";
-import { Link } from "react-router-dom";
-
 
 // import Login from "../Login";
 // import Signup from "../Signup";
-// import {Link} from "react-router-dom";
+import {Link} from "react-router-dom";
 
 class Header extends Component {
   // state = {
-  //   selectedLogin: false
+  //   selectedLogin: undefined
   // }
   // handleLogin = ()=> {
   //   this.setState({
@@ -35,18 +35,6 @@ class Header extends Component {
   //   })
   // }
 
-  isLogin() {
-    if(!localStorage.getItem("profile")) {
-        return false;
-    } else {
-        console.log(localStorage.getItem("profile"))
-        // this.setState({
-        //   selectedLogin: true
-        // })
-        return true;
-    }
-  }
-
   constructor(props) {
         super(props)
         this.auth = new Auth();
@@ -62,30 +50,29 @@ class Header extends Component {
 
   render() {
     // const { isAuthenticated } = this.props.auth;
+
     return (
-        <div className="jumbotron text-center">
+        <div className="jumbotron text-center ">
             {/* <Login closeLogin={this.closeLogin} handleLogin={this.state.selectedLogin} /> */}
             {/* <Signup closeSignup={this.closeSignup} handleSignup={this.state.selectedSignup} handleSignupSubmit={this.handleSignupSubmit} /> */}
-            <h1>SwapCard</h1>
-            <h2>A place to trade your unused and unwanted gift cards for retailer you prefer</h2>
-            {localStorage.getItem("profile") ? <p>Logged in as: {localStorage.getItem("profile")}</p> : ""}
+            <h1>SwapCard <i class="fa fa-credit-card-alt" aria-hidden="true"></i></h1>
+            <h2>A place to trade your unused and unwanted gift cards for retailers you prefer</h2>
             {
               !this.auth.isAuthenticated() && (
                   <Button
                     bsStyle="primary"
-                    className="btn-margin"
+                    className="btn-margin btn-lg"
                     onClick={this.login.bind(this)}
-                  >
-                    Log In / Sign-up
+                  ><i class="fa fa-sign-in" aria-hidden="true"></i>  Log In / <i class="fa fa-user-plus" aria-hidden="true"></i> Sign-up
                   </Button>
                 )
             }
-            {console.log(this.auth)}
+            {console.log(this)}
             {
               this.auth.isAuthenticated() && (
                   <Button
                     bsStyle="primary"
-                    className="btn-margin"
+                    className="btn-margin btn-lg"
                     onClick={this.logout.bind(this)}
                   >
                     Log Out
@@ -93,13 +80,11 @@ class Header extends Component {
                 )
             }
 
-            {this.isLogin() ? <button
-                    className="btn btn-warning" ><Link to="/user">Dashboard</Link></button> : <p></p>}
-
             {/* <button className="btn btn-success" onClick={this.handleLogin}>Log in</button> */}
             {/* <button className="btn btn-danger" onClick={this.handleSignup}>Sign up</button> */}
         </div>
-    )
+
+       )
   }
 }
 

d7155d77a5c89beecd55342b0bbb8c2d1135d343
Sagabootcamp <sagarika.susarla05@gmail.com>
2017-12-07 21:38:48 -0600

    change info

--- client/src/Auth/auth0-variables.js
+++ client/src/Auth/auth0-variables.js
@@ -1,5 +1,5 @@
 export const AUTH_CONFIG = {
-  domain: 'webauth.auth0.com',
-  clientId: 'niFiMEOp4hsdFPv2FyOAnQ8YSPM7R6Y4',
+  domain: 'http://app82573661.auth0.com/',
+  clientId: 'Ab19b6jo7OHyYsEEkZiqtRG9nTiQ3yls',
   callbackUrl: 'https://swapgift.herokuapp.com/callback'
 }

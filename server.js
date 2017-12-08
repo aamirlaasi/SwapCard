48d2de4dc43f63c730694c3e3bcd167db138d278
Aamir Laasi <aamirlaasi@Aamirs-MacBook-Air.local>
2017-12-08 02:30:41 -0600

    m

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ if (process.env.NODE_ENV === "production") {
 // Serve up static assets
 app.use(express.static("client/build"));
 
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '/client/build/index.html'));
 });
 
